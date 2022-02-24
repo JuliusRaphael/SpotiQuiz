d33c568db77dfddc140ff5d107949cc2a195acef
Julius <julius.r.hopf@gmail.com>
2022-02-24 20:12:46 +0100

    Update auth

--- authorize.js
+++ authorize.js
@@ -1,7 +1,7 @@
 async function goToAuth(){
   // Your client id
   var client_id = "client_id=1cb62d180d404900adc7f4b5f5d8ce12";
-  var redirect_uri = "&redirect_uri=https://juliusraphael.github.io/SpotiTop50/redirected.html&scope=user-read-private%20user-read-email&response_type=token&state=123&show_dialog=true"; // Your redirect uri
+  var redirect_uri = "&redirect_uri=https://juliusraphael.github.io/SpotiQuiz/redirected.html&scope=user-read-private%20user-read-email&response_type=token&state=123&show_dialog=true"; // Your redirect uri
   var scope = 'playlist-read-private';
   var url = "https://accounts.spotify.com/authorize?"
   url = url + client_id + redirect_uri;
